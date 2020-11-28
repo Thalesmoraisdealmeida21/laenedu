fb35cedad5f158388d788107f1550f86adcc4ad5
Thales Morais de Almeida <thales.morais21@gmail.com>
2020-11-28 01:31:41 -0300

    Implementado integraçao com a APi

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import './database/connection';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3333, () => {
--- web/src/pages/SeeMore/index.tsx
+++ web/src/pages/SeeMore/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {useHistory} from 'react-router-dom';
 
 import { ContainerHome, LogoContent, FormContainer, ButtonToNext } from './styles';
 
 import logo from './../../assets/logo.png'
+import api from '../../services/api';
 
 
 const Home = () => {
   
 
     const history = useHistory();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = useCallback((data: { name: string; email: string})=> {
+       
+       try {
+        api.post("/reserve_account", data);
+        
+
+        alert("Recebemos seu contato, verifique seu e-mail !");
+      
+       } catch {
+           alert("Ocorreu um erro :(")
+       }
+
+    },[])
 
 
     return (
@@ -23,13 +40,26 @@ const Home = () => {
 
 
                 <FormContainer>     
-                      <form>
+                      <form onSubmit={(evt) => {
+
+                          evt.preventDefault();
+
+                         
+                          handleSubmit( {
+                              email,
+                              name
+                          });
+                      }}>
                           <h1>Interessados ? <br />Saiba Mais:</h1>
                           <span>Nome</span>
-                        <input name="name"></input>
+                        <input name="name" value={name} onChange={(evt)=> {
+                            setName(evt.target.value);
+                        }}></input>
 
                         <span>E-mail</span>
-                        <input name="name"></input>
+                        <input name="email" value={email} onChange={(evt)=> {
+                            setEmail(evt.target.value);
+                        }}></input>
                         <button>
                             Enviar
                         </button>
